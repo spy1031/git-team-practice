e466fe89be42d0e65c1386f01401a6b642712a81
spy <sp831031@gmail.com>
2018-01-27 17:45:08 +0800

    add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+
+10.times do |i|
+  User.create!(name: "user_#{i}")
+  puts "user_#{i} create success."
+end
